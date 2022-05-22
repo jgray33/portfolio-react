2e8ef2df81f4e2739fc2a965f598bdb44851f841
Julia <julia.gray30@gmail.com>
2022-05-22 20:16:08 +0100

    Carousel working

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,33 +1,77 @@
-import React from 'react'
-import projects from '../../projects'
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css"
+const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
-function createCard(project) {
-    return(
-        <Card 
-        key={project.id}
-        name={project.name}
-        description={project.description}
-        siteAddress={project.siteAddress}
-        gitHub={project.gitHub}
-        />
-    )
+const styles = {
+  card: {
+    width: "18rem",
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    objectPosition: 'center'
+  },
+  container: {
+      width: '250px',
+      height: '300px'
+  }
+};
+
+export default function Card({ projects }) {
+     return (
+  <div> 
+    <Carousel responsive={responsive}>
+  {projects.map((project) => (
+<div key={project.id} class="card" style={styles.card} >
+  <img src={project.image} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">{project.name}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  ))}
+  
+</Carousel>
+</div>
+     );
 }
 
 
-export default function Card({projects}) {
-
-    return(
-        <div className="card">
+/* <div className="card flex-container" style={styles.card}>
       <h1>My work</h1>
-      
-        {/* Here we use the map method to iterate through each user and return a new array of list items for each user */}
-        {projects.map((project) => (
-            <div className='card-body' key={project.id}>
-            {`${project.name}, ${project.description}`}
-            <h5 className="card-title">{project.name}</h5>
-            <p className="card-text">{project.description}</p>
-            <a href="{project.site}" className="btn btn-primary">Go somewhere</a>
-            </div>
-        ))}
+      <Carousel responsive={responsive}     >
+      {projects.map((project) => (
+        <div className="card-body" key={project.id}>
+          <h5 className="card-title">{project.name}</h5>
+          <div style={styles.container}>
+              <img style={styles.img} src={project.image}/>
+              </div>
+          <p className="card-text">{project.description}</p>
+          <a href="{project.site}" className="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
-    )}
+      ))}
+      </Carousel>
+    </div>
+  */
--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ const projects = [
         id: 1,
         name: "Weather Dashboard",
         description: "Use this handy weather app to remind you that the grass is absolutely greener and it's only the UK where the weather is miserable",
-        image: "",
+        image: require('./assets/weather-dashboard.png'),
         siteAddress: "https://jgray33.github.io/weather-dashboard/",
         gitHub: "https://github.com/jgray33/weather-dashboard",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
@@ -12,7 +12,7 @@ const projects = [
         id: 2,
         name: "Coding Quiz",
         description: "Say goodbye to boring Saturday nights with this dynamic Coding Quiz. Challenge all your buddies to beat your high score and win the ultimate prize - pride!",
-        image: "",
+        image: require('./assets/coding-quiz.png'),
         siteAddress: "https://jgray33.github.io/coding-quiz/",
         gitHub: "https://github.com/jgray33/coding-quiz",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript', 'Moment', 'Local Storage']
@@ -21,7 +21,7 @@ const projects = [
         id:3,
         name: "Employee Tracker",
         description: "Built in Node.js, this handle little programme allows the user to keep a track of their employees' juicy details such as their salary and job title.",
-        image: "",
+        image: require('./assets/employee-tracker.png'),
         siteAddress: "",
         walkThrough: 'https://www.youtube.com/watch?v=O52OjCItHIU',
         gitHub: "https://github.com/jgray33/employee-tracker",
@@ -31,7 +31,7 @@ const projects = [
         id: 4,
         name: "Caritas",
         description: "A directory for charities - search for a charity to find out what they are up to",
-        image: "",
+        image: require('./assets/caritas.png'),
         siteAddress: "https://aqueous-brook-09754.herokuapp.com/",
         gitHub: "https://github.com/jgray33/charity-directory",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
@@ -40,7 +40,7 @@ const projects = [
         id: 5,
         name: "Blog Site",
         description: "Looking for a place to release all that pent up anger?! Use this anonymous forum to whinge about life. Create yourself a user profile, add posts and comment until your heart's content.",
-        image: "",
+        image: require('./assets/tech-blog.png'),
         siteAddress: "https://pacific-crag-81076.herokuapp.com/",
         gitHub: "https://github.com/jgray33/blog-site",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
@@ -49,7 +49,7 @@ const projects = [
         id: 6,
         name: "Show me more",
         description: "A website that cuts out all the crap when trying to stalk actors from your favorite films. Just search the film and find that actors' key info. Powered by javascript",
-        image: "",
+        image:require('./assets/show-me-more.png') ,
         siteAddress: "https://jgray33.github.io/show-me-more/",
         gitHub: "https://github.com/jgray33/show-me-more",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
