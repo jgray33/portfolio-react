c262aa335cac785a10ffd61afc1b6653cf4033a5
Julia <julia.gray30@gmail.com>
2022-05-24 16:08:50 +0100

    gh pages

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,53 +1,82 @@
-import React, { useState } from 'react';
+import React, {useState} from "react";
 
-function Contact() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = e.target;
-
-    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
-    return name === 'firstName' ? setFirstName(value) : setLastName(value);
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // Alert the user their first and last name, clear the inputs
-    alert(`Hello ${firstName} ${lastName}`);
-    setFirstName('');
-    setLastName('');
-  };
-
-  return (
-    <div>
-      <p>
-        Hello {firstName} {lastName}
-      </p>
-      <form className="form">
-        <input
-          value={firstName}
-          name="firstName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="First Name"
-        />
-        <input
-          value={lastName}
-          name="lastName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Last Name"
-        />
-        <button type="button" onClick={handleFormSubmit}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+const styles = {
+  center: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingTop: "5%",
+  },
+  input: {
+    border: "none",
+    borderBottom: "2px solid white"
+  }
 }
 
-export default Contact;
+export default function Form(){
+
+const [email, setEmail] = useState("");
+const [name, setName] = useState("");
+const [message, setMessage] = useState("")
+const [errorMessage, setErrorMessage] = useState("")
+
+const handleInputChange = (e) => {
+  const {target} = e
+  const inputType = target.name
+  const inputValue = target.inputValue
+
+  if (inputType === "email") {
+    setEmail(inputValue)
+  } else if (inputType === 'name') {
+    setName(inputValue)
+  } else {
+    setMessage(inputType)
+  }
+}
+
+const handleFormSubmit = (e) => {
+  e.preventDefault()
+  
+  if (!email || name || message) {
+    setErrorMessage("Email, name or message is missing")
+    return
+  }
+  setEmail("")
+  setMessage("")
+  setName("")
+}
+
+return(
+    <div className="flex-container">
+    <form className="form">
+      <input style={styles.input}
+      value={name}
+      name="name"
+      onChange={handleInputChange}
+      type="text"
+      placeholder="Name"
+      />
+      <input
+      value={email}
+      name="email"
+      onChange={handleInputChange}
+      type="email"
+      placeholder="email"
+      />
+      <input
+      value={message}
+      name="message"
+      onChange={handleInputChange}
+      type="text"
+      placeholder="Message"
+      />
+      <button type="button" onClick={handleFormSubmit}> Send</button>
+    </form>
+    { errorMessage && (
+      <div>
+        <p className="error-text"> {errorMessage}</p>
+        </div>
+    )}
+  </div>
+)
+}
+
